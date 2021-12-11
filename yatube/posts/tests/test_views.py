2c97fae4c4de8ba514a847700fbf1c5d75d4b968
Кирилл Пугачев <kirill199752@yandex.ru>
2021-12-11 15:53:47 +0300

    result3

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -6,8 +6,7 @@ from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
-
-from posts.models import Comment, Group, Post, User, Follow
+from posts.models import Comment, Follow, Group, Post, User
 
 USER = 'User'
 TITLE = 'Test_title'
